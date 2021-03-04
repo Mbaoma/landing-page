a95de0cf4e8c7a64acef786ca6b2f75cccfd65fb
Ifihan <victoriaolusheye@gmail.com>
2021-03-04 12:47:37 +0100

    Modified to save emails to csv

--- app/views.py
+++ app/views.py
@@ -1,17 +1,17 @@
-from app import app, render_template, request
-from flask import render_template
+from app import app
+from flask import render_template, request
 import csv
 
-def write_to_csv(data):
+def write_to_csv(email: str):
 	with open('database.csv', mode='a') as database:
-		email = data['email']
-		csv_write = csv.writer(database, quotechar='"', quoting=csv.QUOTE_MINIMAL)
-		csv_write.writerow([email])
+		csv_write = csv.writer(database, quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		csv_write.writerow(email)
 
 @app.route("/", methods={"POST", "GET"})
 def index():
 	if request.method == 'POST':
 		email = request.form.get('email')
-		write_to_csv(request.form)
+		print(email)
+		write_to_csv(email)
 		return render_template('thankyou.html')
 	return render_template('index.html')
